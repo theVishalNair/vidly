aa8a7df5e374f36e2de9e472cc5add41047ed902
Unknown <vishal_vijayan@outlook.com>
2019-02-25 20:51:20 +0530

    Updated Index

--- index.js
+++ index.js
@@ -39,15 +39,87 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/genres', (req, res) => {
-    res.send(genres);
+    return res.send(genres);
 });
 
+app.post('/api/genres', (req, res) => {
+    const {
+        error
+    } = validateGenreName(req.body);
+
+
+    if (error) {
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+
+    const genre = {
+        id: genres.length + 1,
+        name: req.body.name
+    };
+    genres.push(genre);
+    return res.status(200).send(genre);
+});
+
+
+app.put('/api/genres/:id', (req, res) => {
+    const {
+        error
+    } = validateGenreName(req.body);
+
+    if (error) {
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+    const id = req.param.id;
+    const index = checkGenreExist(id)
+    if (index == -1) {
+        return res.status(404).send(`Genre with id ${id} does not exist`);
+    }
+
+    genres[index].name = req.body.name;
+    return res.status(200).send(genres[index]);
+});
+
+
+app.delete('api/genres/:id', (req, res) => {
+
+    const id = req.param.id;
+    const index = checkGenreExist(id)
+    if (index == -1) {
+        return res.status(404).send(`Genre with id ${id} does not exist`);
+    }
+
+    const genre = genres[index];
+    return res.status(200).send(genre);
+});
+
+
+
+// Helper Function
+
+validateGenreName = course => {
+    const schema = {
+        name: Joi.string()
+            .min(3)
+            .required()
+    };
+
+    return Joi.validate(course, schema);
+};
+
+checkGenreExist = id => {
+    const index = genres.findIndex(c => {
+        return c.id == parseInt(id);
+    })
+    return index;
+}
 
 
 
 // Creation of a server
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
     console.log(`App has been started on port ${port}`);
